de643362806572215b3d21d2ec5d7233117c92b6
microwerx <jmetzgar@outlook.com>
2019-10-18 13:05:05 -0800

    add get method

--- src/XorObjectManager.ts
+++ src/XorObjectManager.ts
@@ -12,7 +12,7 @@ namespace XOR {
     export class GameObjectManager {
         private arrayInfo = new Map<number, ArrayIndexInfo>();
         private length_ = 0;
-        private objects: GravityObject[];
+        private objects = new Array<GameObject>();
 
         unknownObject = new GameObject(this);
 
@@ -21,6 +21,21 @@ namespace XOR {
         add(index: number, count: number) {
             this.arrayInfo.set(index | 0,
                 new ArrayIndexInfo(this.objects.length, count));
+            this.objects.length = this.objects.length + count;
+        }
+
+        get(index: number, i: number): GameObject {
+            if (!this.arrayInfo.has(index)) {
+                return this.unknownObject;
+            }
+            let ainfo = this.arrayInfo.get(index);
+            if (i < 0 && i >= ainfo.length) { return this.unknownObject; }
+            let obj = this.objects[i + ainfo.first];
+            if (!obj) {
+                hflog.error("Object " + index + "/" + i + " does not exist!");
+                return this.unknownObject;
+            }
+            return obj;
         }
     }
 }
